bf7b64973fb57a0178cab7a7aa2884a4e9113dc0
Bogdan Mitru Alexandru <bogdan.mitru@student.upt.ro>
2024-11-05 12:43:54 +0200

    made copy and move constructors for MediaFile

--- MediaFile.cpp
+++ MediaFile.cpp
@@ -4,13 +4,33 @@
 // Constructor
 MediaFile::MediaFile(const std::string& title, const std::string& author, uint16_t year)
     : title(new std::string(title)), author(new std::string(author)), publication_year(year) {
-    std::cout << "MediaFile constructor called" << std::endl;
+    std::cout << "MediaFile Constructor called" << std::endl;
 //    display_info();
+}
+
+// Copy Constructor
+MediaFile::MediaFile(const MediaFile& other)
+    : title(new std::string(*other.title)), // Deep copy title
+    author(new std::string(*other.author)), // Deep copy author
+    publication_year(other.publication_year) {
+    std::cout << "MediaFile Copy Constructor called" << std::endl;
+}
+
+// Move Constructor
+MediaFile::MediaFile(const MediaFile&& other) noexcept
+    : title(other.title),
+    author(other.author),
+    publication_year(other.publication_year) {
+    std::cout << "MediaFile Move Constructor called" << std::endl;
+
+    // Set other's pointers to nullptr to avoid double deletion
+    other.title = nullptr;
+    other.author = nullptr;
 }
 
 // Destructor
 MediaFile::~MediaFile() {
-    std::cout << "MediaFile destructor called" << std::endl;
+    std::cout << "MediaFile Destructor called" << std::endl;
     delete title;   // Free memory allocated for title
     delete author;  // Free memory allocated for author
 }
--- MediaFile.h
+++ MediaFile.h
@@ -6,13 +6,20 @@
 
 class MediaFile {
 public:
+    // Constructor
     MediaFile(const std::string& title, const std::string& author, uint16_t year);
+    // Copy Constructor
+    MediaFile(const MediaFile& other);
+    // Move Constructor
+    MediaFile(const MediaFile&& other);
+    // Destructor
     virtual ~MediaFile(); // Make destructor virtual
+    // method for displaying basic information about the file
     virtual void display_info() const; // Declare display_info as a virtual function
 
 private:
-    std::string* title; // Pointer member variable
-    std::string* author; // Pointer member variable
+    mutable std::string* title; // Pointer member variable
+    mutable std::string* author; // Pointer member variable
     uint16_t publication_year;
 };
 
